5f90fa1f08390f283a1a0cbfeeb51f496718caa6
Aniket Inamdar <aniketinamdar@Anikets-MacBook-Pro.local>
2024-12-11 10:18:22 +0530

    made changes

--- main.py
+++ main.py
@@ -130,6 +130,6 @@ async def execute_run():
     result = run()
     return result
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT",8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT",8000))
+#     uvicorn.run(app, host="0.0.0.0", port=port)
